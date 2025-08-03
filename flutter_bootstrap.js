b667d62a1bdcd7c70d1ec1adbaebf63eaf2b5967
ABDELRAHMAN MOHAMED <amohamed21@ku.edu.tr>
2025-08-03 13:20:31 +0300

    d

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2942109876"
+    serviceWorkerVersion: "307232941"
   }
 });
